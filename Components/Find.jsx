ba0ead3ec387536707bfb8f690c06ebf376a081a
Muhammad Anwaar <anwarOfficial9@gmail.com>
2024-03-28 02:54:47 +0500

    this is my first commit

---
+++ Components/Find.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Find = () => {
+  return (
+    <div>
+<h1 className='flex justify-center font-bold text-2xl'>Paient Detail</h1>
+
+<button  className='bg-red-500 border-2 '>Click Me</button>
+
+
+    </div>
+  )
+}
+
+export default Find
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  backgroundImage: {
+    'hero-pattern': "url('/img/hero-pattern.svg')",
+    'footer-texture': "url('/img/footer-texture.png')",
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
