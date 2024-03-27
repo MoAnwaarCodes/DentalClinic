e33e8a50a29cfb6a0f945bd07fe05b195d5049c9
Muhammad Anwaar <anwarOfficial9@gmail.com>
2024-03-28 04:51:42 +0500

    Fetched all the data from data base on bases of ContactNo

--- Components/Find.jsx
+++ Components/Find.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Find = () => {
+ 
   return (
     <div>
+      
 <h1 className='flex justify-center font-bold text-2xl'>Paient Detail</h1>
 
-<button  className='bg-red-500 border-2 '>Click Me</button>
+<button  className='bg-red-500 border-2 ' onClick={()=>{
+
+
+}}>Click Me</button>
 
 
     </div>
